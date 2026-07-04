4650b474d7f3943fca91ce2c13f23e698af64dc3
hokke414 <hokke41499@gmail.com>
2026-07-04 22:48:56 +0900

    template

---
+++ src/data/templates.ts
@@ -0,0 +1,16 @@
+export type TemplateItem = {
+  title: string;
+  description: string;
+  instagramUrl: string;
+  templateUrl: string;
+};
+
+export const templates: TemplateItem[] = [
+  // テンプレートを追加するにはここにオブジェクトを追記してください。
+  // {
+  //   title: 'テンプレートのタイトル',
+  //   description: 'テンプレートの簡単な説明',
+  //   instagramUrl: 'https://www.instagram.com/p/XXXXXXXXX/',
+  //   templateUrl: 'https://www.notion.so/templates/XXXXXXXXX',
+  // },
+];
