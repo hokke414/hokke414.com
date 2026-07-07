50014c90e6e3550bac82cb0d02a73062f2ac4427
hokke414 <hokke41499@gmail.com>
2026-07-07 22:27:35 +0900

    note

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 import { remarkLinkCard } from './src/plugins/remark-link-card.js';
 
 export default defineConfig({
   site: 'https://hokke414.com',
+  adapter: vercel(),
   integrations: [sitemap()],
   markdown: {
     remarkPlugins: [remarkLinkCard],
